3640bdfac5ba9f033e5c15d34c1b8268e7018760
Kenne Serge <kenne_serge@mail.ru>
2020-01-01 23:52:54 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,38 @@
+//function refresh page in body 
+// function refresh() {
+
+//    location.reload(); 
+
+// }
+
+$(window).resize(function(){
+	location.reload();
+})
+
+
+
+
+
+var windowWidth = $(window).width();
+
+if(windowWidth < 769) {
+	$(".js-slide-btn").click(function() {
+		$(".js-sublist").slideToggle();
+		
+	});
+}
+
+
+$(".nav-menu__link, .nav-menu__subList-link, .left-side__link").click(function(){
+	var inner_text = $(this).html();
+    $(".header__slide-btn").html(inner_text);
+	
+});
+
+
+// $(".products").click(function() {
+// 	var products_name = $(this).find(".products__title").html();
+// 	//$(".categories-title").html() = products_name;
+// 	alert($(".categories-title").html());
+// });
+
